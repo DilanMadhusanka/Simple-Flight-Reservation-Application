19a2a6f2170c52dc10cc3e80f5e64a2899486e26
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-02-06 08:29:39 +0530

    implement updateReservation method

--- Flight-CheckIn-Application/src/main/java/com/application/flightcheckin/FlightCheckInApplication/integration/ReservationRestClientImpl.java
+++ Flight-CheckIn-Application/src/main/java/com/application/flightcheckin/FlightCheckInApplication/integration/ReservationRestClientImpl.java
@@ -7,17 +7,20 @@ import com.application.flightcheckin.FlightCheckInApplication.dto.ReservationUpd
 
 public class ReservationRestClientImpl implements ReservationRestClient {
 
+	private static final String RESERVATIONS_REST_URL = "http://localhost:8080/reservations/";
+
 	@Override
 	public Reservation findReservation(Long id) {
 		RestTemplate restTemplate = new RestTemplate();
-		Reservation reservation = restTemplate.getForObject("http://localhost:8080/reservation/"+id, Reservation.class);
+		Reservation reservation = restTemplate.getForObject(RESERVATIONS_REST_URL + id, Reservation.class);
 		return reservation;
 	}
 
 	@Override
 	public Reservation updateReservation(ReservationUpdateRequest request) {
-		// TODO Auto-generated method stub
-		return null;
+		RestTemplate restTemplate = new RestTemplate();
+		Reservation reservation = restTemplate.postForObject(RESERVATIONS_REST_URL, request, Reservation.class);
+		return reservation;
 	}
 
 }
