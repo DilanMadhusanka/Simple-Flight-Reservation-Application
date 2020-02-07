f996d130eb0e3bd2ebec2178b3f3955337f2b9fb
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-02-07 10:25:12 +0530

    get the id from startCheckIn jsp to contoller class and set to the rest client and pass the details to displayReservation jsp

--- Flight-CheckIn-Application/src/main/java/com/application/flightcheckin/FlightCheckInApplication/controller/CheckInController.java
+++ Flight-CheckIn-Application/src/main/java/com/application/flightcheckin/FlightCheckInApplication/controller/CheckInController.java
@@ -1,13 +1,29 @@
 package com.application.flightcheckin.FlightCheckInApplication.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.application.flightcheckin.FlightCheckInApplication.dto.Reservation;
+import com.application.flightcheckin.FlightCheckInApplication.integration.ReservationRestClient;
 
 @Controller
 public class CheckInController {
 
+	@Autowired
+	ReservationRestClient restClient;
+	
 	@RequestMapping("/showStartCheckIn")
 	public String showStartCheckIn() {
 		return "startCheckIn";
+	}
+	
+	@RequestMapping("/startCheckIn")
+	public String startCheckIn(@RequestParam("reservationId") Long reservationId, ModelMap modelMap) {
+		Reservation reservation = restClient.findReservation(reservationId);
+		modelMap.addAttribute("reservation", reservation);
+		return "displayReservationDetails";
 	}
 }
--- Flight-CheckIn-Application/src/main/java/com/application/flightcheckin/FlightCheckInApplication/integration/ReservationRestClientImpl.java
+++ Flight-CheckIn-Application/src/main/java/com/application/flightcheckin/FlightCheckInApplication/integration/ReservationRestClientImpl.java
@@ -1,10 +1,12 @@
 package com.application.flightcheckin.FlightCheckInApplication.integration;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import com.application.flightcheckin.FlightCheckInApplication.dto.Reservation;
 import com.application.flightcheckin.FlightCheckInApplication.dto.ReservationUpdateRequest;
 
+@Component
 public class ReservationRestClientImpl implements ReservationRestClient {
 
 	private static final String RESERVATIONS_REST_URL = "http://localhost:8080/reservations/";
