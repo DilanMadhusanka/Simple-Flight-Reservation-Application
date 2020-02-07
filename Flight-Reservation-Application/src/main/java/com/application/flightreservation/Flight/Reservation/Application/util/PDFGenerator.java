cf2dd1ba5a357e7126631fad23beeeb23a83f121
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-02-07 23:03:29 +0530

    add flight details

--- Flight-Reservation-Application/src/main/java/com/application/flightreservation/Flight/Reservation/Application/util/PDFGenerator.java
+++ Flight-Reservation-Application/src/main/java/com/application/flightreservation/Flight/Reservation/Application/util/PDFGenerator.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Component;
 import com.application.flightreservation.Flight.Reservation.Application.entities.Reservation;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+//import com.itextpdf.text.Element;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
@@ -20,10 +23,41 @@ public class PDFGenerator {
 		try {
 			PdfWriter.getInstance(document, new FileOutputStream(filePath));
 			document.open(); 
-			document.add(new PdfPTable(2));
+//			document.add(new PdfPTable(2));
+			document.add(generateTable(reservation));
 			document.close();
 		} catch (FileNotFoundException | DocumentException e) {
 			e.printStackTrace();
 		}
+	}
+
+	private PdfPTable generateTable(Reservation reservation) {
+		PdfPTable table = new PdfPTable(2);
+		
+		PdfPCell cell;
+		cell = new PdfPCell(new Phrase("Flight Itinerary"));
+		cell.setColspan(2);
+		table.addCell(cell);
+		
+		cell = new PdfPCell(new Phrase("Flight Details"));
+		cell.setColspan(2);
+		table.addCell(cell);
+		
+		table.addCell("Departure City");
+		table.addCell(reservation.getFlight().getDepartureCity());
+		
+		table.addCell("Arrival City");
+		table.addCell(reservation.getFlight().getArrivalCity());
+		
+		table.addCell("Flight Number");
+		table.addCell(reservation.getFlight().getFlightNumber());
+		
+		table.addCell("Departure Date");
+		table.addCell(reservation.getFlight().getDateOfDeparture().toString());
+		
+		table.addCell("Departure Time");
+		table.addCell(reservation.getFlight().getEstimatedDepartureTime().toString());
+		
+		return table;
 	}
 }
